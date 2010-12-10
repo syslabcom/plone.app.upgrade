3962ab267bfc6951746f8d7f094e6fbfb2979c98
Denys Mishunov <denys.mishunov@gmail.com>
2010-12-10 09:13:15 +0000

    Updated 4.0-4.1alpha upgrade step to not check for search.js being registered, but rather for plone.app.search being installed. Testing for actual js file is happening in that package. References #9352
    svn path=/plone.app.upgrade/branches/plip9352-search-ui/; revision=46233

--- plone/app/upgrade/v41/__init__.py
+++ plone/app/upgrade/v41/__init__.py
@@ -1,6 +1,6 @@
 from plone.app.upgrade.utils import loadMigrationProfile
 
-def four0_four1_a1(context): 
+def four0_four1_a1(context):
     """4.0.x -> 4.1a1
-    """    
+    """
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v41:4.0.x-4.1a1')
--- plone/app/upgrade/v41/tests.py
+++ plone/app/upgrade/v41/tests.py
@@ -5,21 +5,29 @@ from plone.app.upgrade.tests.base import MigrationTest
 
 from Products.ResourceRegistries.config import JSTOOLNAME
 
+
 class TestMigrations_v4_1alpha1(MigrationTest):
 
     profile = 'profile-plone.app.upgrade.v41:4.0.x-4.1a1'
+
+    def afterSetUp(self):
+        self.jstool = getToolByName(self.portal, JSTOOLNAME)
 
     def testProfile(self):
         # This tests the whole upgrade profile can be loaded
         loadMigrationProfile(self.portal, self.profile)
         self.failUnless(True)
 
-    def afterSetUp(self):
-        self.tool = getToolByName(self.portal, JSTOOLNAME)
-
-    def testDefaultJSIsInstalled(self):
-        installedScriptIds = self.tool.getResourceIds()
-        self.failUnless('search.js' in installedScriptIds)
+    def testInstallNewDependencies(self):
+        # test for running the plone.app.search profile by checking for the js
+        # file it installs (the profile is marked as noninstallable, so we
+        # can't ask the quick installer)
+        installedScriptIds = self.jstool.getResourceIds()
+        expected = [
+            # js resource that is part of plone.app.search
+            '++resource++search.js']
+        for e in expected:
+            self.failUnless(e in installedScriptIds, e)
 
 
 def test_suite():
